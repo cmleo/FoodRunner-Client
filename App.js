815eff9a9db847cd151c680c34ffdc508fcefdf5
cmleo <leo.marinel13@yahoo.com>
2023-04-03 21:24:33 +0300

    created basic register screen

--- App.js
+++ App.js
@@ -1,21 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import RegisterScreen from './src/screens/Register/RegisterScreen';
 
 export default function App() {
 	return (
-		<View style={styles.container}>
-			<Text>Food Runner. A Cross-Platform Food Delivery Mobile App</Text>
-			<Text>Food Runner = The Love for Food and Run</Text>
-			<StatusBar style='auto' />
+		<View>
+			<RegisterScreen />
 		</View>
 	);
 }
-
-const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		backgroundColor: '#fff',
-		alignItems: 'center',
-		justifyContent: 'center',
-	},
-});
