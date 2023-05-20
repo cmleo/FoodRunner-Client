6bbc2242ad4a67bbc14c8544e62b4c6aaa7672c2
cmleo <leo.marinel13@yahoo.com>
2023-05-20 15:07:58 +0300

    created footer component

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { View, StyleSheet, TouchableOpacity } from 'react-native';
+import { AntDesign, Ionicons, MaterialIcons, FontAwesome } from '@expo/vector-icons';
+import { useNavigation } from '@react-navigation/native';
+
+const Footer = () => {
+	const navigation = useNavigation();
+	return (
+		<View style={styles.footer}>
+			<TouchableOpacity onPress={() => navigation.navigate('Home')}>
+				<AntDesign name='home' size={24} color='black' />
+			</TouchableOpacity>
+			<TouchableOpacity>
+				<Ionicons name='search' size={24} color='black' />
+			</TouchableOpacity>
+			<TouchableOpacity onPress={() => navigation.navigate('Cart')}>
+				<MaterialIcons name='shopping-cart' size={24} color='black' />
+			</TouchableOpacity>
+			<TouchableOpacity onPress={() => navigation.navigate('Dashboard')}>
+				<FontAwesome name='user' size={24} color='black' />
+			</TouchableOpacity>
+		</View>
+	);
+};
+
+const styles = StyleSheet.create({
+	footer: {
+		flexDirection: 'row',
+		justifyContent: 'space-around',
+		alignItems: 'center',
+		backgroundColor: '#fff',
+		height: 60,
+		borderTopWidth: 1,
+		borderTopColor: '#ccc',
+		width: '100%',
+	},
+});
+
+export default Footer;
