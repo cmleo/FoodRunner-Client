9b186ef117a206a43ee785837121a1c7d468071d
cmleo <leo.marinel13@yahoo.com>
2023-05-23 19:28:19 +0300

    changed the default style with tailwindcss

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,7 +1,8 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import React, { useState, useEffect } from 'react';
-import { SafeAreaView, ScrollView, StyleSheet, Text, View } from 'react-native';
+import { Text, View, ScrollView } from 'react-native';
 import Footer from '../Footer/Footer';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 function Dashboard() {
 	const [userData, setUserData] = useState({});
@@ -9,7 +10,7 @@ function Dashboard() {
 	useEffect(() => {
 		const fetchUserData = async () => {
 			const token = await AsyncStorage.getItem('token');
-			const response = await fetch(`http://192.168.0.101:3000/user`, {
+			const response = await fetch(`http://192.168.0.102:3000/user`, {
 				headers: {
 					Authorization: `Bearer ${token}`,
 				},
@@ -21,28 +22,22 @@ function Dashboard() {
 	}, []);
 
 	return (
-		<SafeAreaView>
-			<Text>Dashboard</Text>
+		<SafeAreaView className='flex-1 mt-1'>
+			<ScrollView className='flex-grow pb-16'>
+				<Text className='text-xl font-bold mb-4'>Dashboard</Text>
 
-			{userData ? (
-				<>
-					<Text style={styles.text}>Welcome, {userData.name}!</Text>
-				</>
-			) : (
-				<Text>Loading...</Text>
-			)}
+				{userData ? (
+					<>
+						<Text className='text-lg'>Welcome, {userData.name}!</Text>
+					</>
+				) : (
+					<Text className='text-lg'>Loading...</Text>
+				)}
+			</ScrollView>
 
 			<Footer />
 		</SafeAreaView>
 	);
 }
-
-const styles = StyleSheet.create({
-	text: {
-		fontSize: 20,
-		fontWeight: 'bold',
-		color: '#333',
-	},
-});
 
 export default Dashboard;
