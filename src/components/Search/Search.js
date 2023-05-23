754d63abb2a7242061aecd9e22ff15c5cb9fc2ae
cmleo <leo.marinel13@yahoo.com>
2023-05-23 18:14:19 +0300

    created search component

---
+++ src/components/Search/Search.js
@@ -0,0 +1,16 @@
+import { View, Text, TextInput } from 'react-native';
+import React from 'react';
+import { Feather, Ionicons } from '@expo/vector-icons';
+
+export default function Search() {
+	return (
+		<View className='flex-row flex-1 items-center p-3 rounded-full border border-gray-300'>
+			<Ionicons name='search' size={24} color='black' />
+			<TextInput placeholder='Restaurants' className='flex-1 ml-2' />
+			<View className='flex-row items-center space-x-1 border-0 border-l-2 pl-2 border-l-gray-300'>
+				<Feather name='map-pin' size={24} color='black' />
+				<Text className='text-gray-600'></Text>
+			</View>
+		</View>
+	);
+}
