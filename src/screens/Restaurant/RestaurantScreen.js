61f551b5b21ad87bd7004b8b5ae0c52e0d188500
cmleo <leo.marinel13@yahoo.com>
2023-05-28 18:29:44 +0300

    changed the default style with tailwindcss classnames

--- src/screens/Restaurant/RestaurantScreen.js
+++ src/screens/Restaurant/RestaurantScreen.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { View, Text, ScrollView, Image, StyleSheet } from 'react-native';
+import { View, Text, ScrollView, Image } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import Footer from '../../components/Footer/Footer';
 import { useRoute } from '@react-navigation/native';
@@ -17,7 +17,6 @@ export default function RestaurantScreen() {
 				const response = await fetch(`${env.API_URL}/restaurants/${restaurantId}`);
 				const data = await response.json();
 				setRestaurant(data.restaurant);
-				console.log(data);
 			} catch (error) {
 				console.log(error);
 			}
@@ -31,26 +30,26 @@ export default function RestaurantScreen() {
 	}
 
 	return (
-		<SafeAreaView style={styles.container}>
-			<ScrollView style={styles.scrollView} contentContainerStyle={styles.contentContainer}>
+		<SafeAreaView className='flex-1'>
+			<ScrollView className='flex-1' contentContainerStyle='py-20 px-10'>
 				{/* Restaurant logo, name, and location */}
-				<View style={styles.restaurantHeader}>
-					<Image source={{ uri: restaurant.logo }} style={styles.logo} />
-					<Text style={styles.restaurantName}>{restaurant.restaurantName}</Text>
-					<Text style={styles.location}>{restaurant.location}</Text>
+				<View className='items-center mb-20'>
+					<Image source={{ uri: restaurant.logo }} className='w-40 h-40 rounded-full' />
+					<Text className='text-2xl font-bold mt-4'>{restaurant.restaurantName}</Text>
+					<Text className='text-lg mt-2'>{restaurant.location}</Text>
 				</View>
 
 				{/* Menu */}
-				<View style={styles.menuContainer}>
-					<Text style={styles.menuTitle}>Menu</Text>
+				<View className='mb-20'>
+					<Text className='text-2xl font-bold mb-4 ml-2'>Menu</Text>
 					{restaurant.menu ? (
 						restaurant.menu.map((item, index) => (
-							<View key={index} style={styles.menuItem}>
-								<Image source={{ uri: item.image }} style={styles.menuItemImage} />
-								<View style={styles.menuItemDetails}>
-									<Text style={styles.menuItemName}>{item.productName}</Text>
-									<Text style={styles.menuItemDescription}>{item.description}</Text>
-									<Text style={styles.menuItemPrice}>${item.price}</Text>
+							<View key={index} className='flex-row items-center mb-4'>
+								<Image source={{ uri: item.image }} className='w-20 h-20 rounded-md mr-4 ml-2' />
+								<View className='flex-1'>
+									<Text className='text-xl font-bold'>{item.productName}</Text>
+									<Text className='text-base mb-2 mr-1'>{item.description}</Text>
+									<Text className='text-lg font-bold'>${item.price}</Text>
 								</View>
 							</View>
 						))
@@ -63,69 +62,3 @@ export default function RestaurantScreen() {
 		</SafeAreaView>
 	);
 }
-
-const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		marginTop: 1,
-	},
-	scrollView: {
-		flex: 1,
-	},
-	contentContainer: {
-		paddingVertical: 20,
-		paddingHorizontal: 10,
-	},
-	restaurantHeader: {
-		alignItems: 'center',
-		marginBottom: 20,
-	},
-	logo: {
-		width: 100,
-		height: 100,
-		borderRadius: 50,
-	},
-	restaurantName: {
-		fontSize: 24,
-		fontWeight: 'bold',
-		marginTop: 10,
-	},
-	location: {
-		fontSize: 16,
-		marginTop: 5,
-	},
-	menuContainer: {
-		marginBottom: 20,
-	},
-	menuTitle: {
-		fontSize: 20,
-		fontWeight: 'bold',
-		marginBottom: 10,
-	},
-	menuItem: {
-		flexDirection: 'row',
-		alignItems: 'center',
-		marginBottom: 10,
-	},
-	menuItemImage: {
-		width: 80,
-		height: 80,
-		borderRadius: 10,
-		marginRight: 10,
-	},
-	menuItemDetails: {
-		flex: 1,
-	},
-	menuItemName: {
-		fontSize: 18,
-		fontWeight: 'bold',
-	},
-	menuItemDescription: {
-		fontSize: 14,
-		marginBottom: 5,
-	},
-	menuItemPrice: {
-		fontSize: 16,
-		fontWeight: 'bold',
-	},
-});
