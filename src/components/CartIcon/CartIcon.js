a7c9c398211d71ad13cfce22035843eeb7f56fb7
cmleo <leo.marinel13@yahoo.com>
2023-06-14 19:42:27 +0300

    added useState and useEffect hooks

--- src/components/CartIcon/CartIcon.js
+++ src/components/CartIcon/CartIcon.js
@@ -1,11 +1,20 @@
 import { View, Text, TouchableOpacity } from 'react-native';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { themeColors } from '../../theme/theme';
 import { useNavigation } from '@react-navigation/native';
 
 export default function CartIcon({ cartItems, restaurantId }) {
-	const totalQuantity = Object.values(cartItems).reduce((total, item) => total + item.quantity, 0);
-	const totalPrice = Object.values(cartItems).reduce((total, item) => total + item.price * item.quantity, 0);
+	const [totalQuantityState, setTotalQuantityState] = useState(0);
+	const [totalPriceState, setTotalPriceState] = useState(0);
+
+	useEffect(() => {
+		const totalQuantity = Object.values(cartItems).reduce((total, item) => total + item.quantity, 0);
+		setTotalQuantityState(totalQuantity);
+
+		const totalPrice = Object.values(cartItems).reduce((total, item) => total + item.price * item.quantity, 0);
+		setTotalPriceState(totalPrice);
+	}, [cartItems]);
+
 	const navigation = useNavigation();
 
 	return (
@@ -18,10 +27,10 @@ export default function CartIcon({ cartItems, restaurantId }) {
 				}}
 			>
 				<View className='p-2 px-4 rounded-full' style={{ backgroundColor: 'rgba(255, 255, 255, 0.3)' }}>
-					<Text className='font-extrabold text-white text-lg'>{totalQuantity}</Text>
+					<Text className='font-extrabold text-white text-lg'>{totalQuantityState}</Text>
 				</View>
 				<Text className='flex-1 text-center font-extrabold text-white text-lg'>View Cart</Text>
-				<Text className='font-extrabold text-white text-lg'>${totalPrice}</Text>
+				<Text className='font-extrabold text-white text-lg'>${totalPriceState}</Text>
 			</TouchableOpacity>
 		</View>
 	);
